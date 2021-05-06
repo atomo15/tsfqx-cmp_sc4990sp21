68eacaa343c24ff85478597fdc56d623781f77ea
Thunnathorne Synhiranakkrakul <49804761+atomo15@users.noreply.github.com>
2021-05-05 20:42:44 -0500

    Update Ptz and Body camera control

--- tsfqx_project.py
+++ tsfqx_project.py
@@ -1,67 +1,114 @@
+
 import bosdyn.client
+
+from pynput import keyboard
+from pynput.keyboard import Key
+from pynput.keyboard import Listener
 
 from bosdyn.client.image import ImageClient #get ImageClient instance
 
+#register spot cam for Ptz
 from bosdyn.client.spot_cam.ptz import PtzClient
+from bosdyn.client import spot_cam
+from bosdyn.api.spot_cam import ptz_pb2
+#register spot cam for Ptz
+
+#Ptz capture
+from bosdyn.client.spot_cam.media_log import MediaLogClient
+from bosdyn.api import image_pb2
+from bosdyn.api.spot_cam import logging_pb2, camera_pb2
+#Ptz capture
 
 from PIL import Image #open image file
 import io
 
 import inspect
 
-from pynput import keyboard
-from pynput.keyboard import Key
+
 
 
 #print(dir(PtzClient.get_ptz_position))
 
 #print(inspect.getfullargspec(PtzClient.get_ptz_position))
 
-horizontal = int(0)
+#Set main menu
 default_menu = "1: Image | 2: Ptz"
 select_menu = ""
-vertical = int(0)
-zoom = int(0)
+#Set main menu
 
+
+
+
+#set up choice for camera
 image_source = ['back_depth', 'back_depth_in_visual_frame', 'back_fisheye_image', 'frontleft_depth', 'frontleft_depth_in_visual_frame', 'frontleft_fisheye_image', 'frontright_depth', 'frontright_depth_in_visual_frame', 'frontright_fisheye_image', 'left_depth', 'left_depth_in_visual_frame', 'left_fisheye_image', 'right_depth', 'right_depth_in_visual_frame', 'right_fisheye_image']
+#set up choice for camera
 
-camera = ""
+#Set default camera and camera index
+camera = "back_depth"
 c_index = int(0)
+#Set default camera and camera index
 
 print("Create Robot")
-#sdk = bosdyn.client.create_standard_sdk('understanding-spot')
-#
-#robot = sdk.create_robot('192.168.80.3')
-#
-#id_client = robot.ensure_client('robot-id')
-#
+
+sdk = bosdyn.client.create_standard_sdk('understanding-spot')
+
+#register spot cam for Ptz
+spot_cam.register_all_service_clients(sdk)
+#register spot cam for Ptz
+
+robot = sdk.create_robot('192.168.80.3')
+
+id_client = robot.ensure_client('robot-id')
+
 #print(id_client.get_id())
-#
-#fut = id_client.get_id_async()
-#
+
+fut = id_client.get_id_async()
+
 #print(fut.result())
-#
-#robot.authenticate('atom', 'atom29589990')
-#
-#state_client = robot.ensure_client('robot-state')
-#
-#state_bot = state_client.get_robot_state()
 
-#image_client = robot.ensure_client(ImageClient.default_service_name)
-#sources = image_client.list_image_sources()
-#[source.name for source in sources]
-#['back_depth', 'back_depth_in_visual_frame', 'back_fisheye_image', 'frontleft_depth', 'frontleft_depth_in_visual_frame', 'frontleft_fisheye_image', 'frontright_depth', 'frontright_depth_in_visual_frame', 'frontright_fisheye_image', 'left_depth', 'left_depth_in_visual_frame', 'left_fisheye_image', 'right_depth', 'right_depth_in_visual_frame', 'right_fisheye_image']
+robot.authenticate('atom', 'atom29589990')
+
+state_client = robot.ensure_client('robot-state')
+
+state_bot = state_client.get_robot_state()
+
+print(state_bot)
+
+#Set up image instance
+image_client = robot.ensure_client(ImageClient.default_service_name)
+sources = image_client.list_image_sources()
+#Set up image instance
+
+#Set up Ptz
+
+#Store pan tilt zoom values
+horizontal = int(0)
+vertical = int(0)
+zoom = int(0)
+#Store pan tilt zoom values
+
+option_name = "mech"
+ptz_desc = ptz_pb2.PtzDescription(name=option_name)
+ptz_client = robot.ensure_client(PtzClient.default_service_name)
+#Set up Ptz
+
+#Ptz capture
+medialog_client = robot.ensure_client(MediaLogClient.default_service_name)
+#options = Namespace(camera_name='ptz', command='media_log', dst=None, hostname='192.168.80.3', media_log_command='store_retrieve', password='atom29589990', save_as_rgb24=False, stitching=True, username='atom', verbose=False)
+#options = [camera_name='ptz',command='media_log',dst=None,hostname='192.168.80.3',media_log_command='store_retrieve',password='atom29589990',save_as_rgb24=False,stitching=True,username='atom',verbose=False]
 #
-#image_response = image_client.get_image_from_sources(["left_fisheye_image"])[0]
+#args = (camera_pb2.Camera(name="ptz"), logging_pb2.Logpoint.STILLIMAGE)
 #
-#image = Image.open(io.BytesIO(image_response.shot.image.data))
-#image.show()
+#lp = client.store(*args)
+#
+#while lp.status != logging_pb2.Logpoint.COMPLETE:
+#    lp = client.get_status(lp)
+#
+#lp = MediaLogRetrieveCommand.save_logpoint_as_image(robot, lp, options, dst_filename=None)
+
+#Ptz capture
 
 
-    #print(PtzClient)
-    #ptz_client = robot.ensure_client(PtzClient.default_service_name)
-
-    #print(ptz_client.get_ptz_position())
 
 
 def save(x,y,z):
@@ -76,9 +123,12 @@ def reset():
     global zoom
     global vertical
     global horizontal
+    global ptz_client
+    global ptz_desc
     zoom = 0
     vertical = 0
     horizontal = 0
+    ptz_position = ptz_client.set_ptz_position(ptz_desc,0,0,0)
 
     
 def get():
@@ -98,6 +148,8 @@ def on_press(key):
     global camera
     global c_index
     global image_source
+    global ptz_desc
+    global ptz_client
     print("Menu: ",default_menu)
     r = False
     c = False
@@ -106,14 +158,14 @@ def on_press(key):
         #print('Alphanumeric key pressed: {0} '.format(
         #    key.char))
         if key.char == '=':
-            if select_menu == " Ptz":
-                print("Zooming in")
+            if select_menu == " Ptz":#print("Zooming in")
+                ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                 z+=1
                 c = True
         elif key.char == '-':
-            if select_menu == " Ptz":
+            if select_menu == " Ptz":#print("Zooming out")
                 if z>0:
-                    print("Zooming out")
+                    ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                     z-=1
                     c = True
         elif key.char == 'w':
@@ -133,42 +185,43 @@ def on_press(key):
         #print('special key pressed: {0}'.format(
         #    key))
         if key == Key.up:
-            #print("Turn Spot-cam UP")
-            if select_menu == " Ptz":
+            if select_menu == " Ptz":#print("Turn Spot-cam UP")
                 if y >-90 and y <90:
                     y+=1
+                    ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                     c = True
         elif key == Key.down:
-            #print("Turn Spot-cam Down")
-            if select_menu == " Ptz":
+            if select_menu == " Ptz":#print("Turn Spot-cam Down")
                 if y>-90:
                     y-=1
+                    ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                     c = True
         elif key == Key.left:
-            #print("Turn Spot-cam left")
-            if select_menu == " Ptz":
+            if select_menu == " Ptz":#print("Turn Spot-cam left")
                 if x>0:
                     x-=1
+                    ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                     c = True
             elif select_menu == " Image":
                 c_index = c_index - 1
                 if c_index <0:
                     c_index = 0
                 camera = image_source[c_index]
-                print("previous camera view",camera)
+                print("Camera View : ",camera)
                 ###
         elif key == Key.right:
-            #print("Turn Spot-cam right")
-            if select_menu == " Ptz":
+            if select_menu == " Ptz":#print("Turn Spot-cam right")
                 if x >= 0 and x <=360:
                     x+=1
+                    #global ptz_client
+                    ptz_position = ptz_client.set_ptz_position(ptz_desc,x,y,z)
                     c = True
             elif select_menu == " Image":
                 c_index = c_index + 1
                 if c_index == len(image_source):
                     c_index = 0
                 camera = image_source[c_index]
-                print("next camera view",camera)
+                print("Camera View : ",camera)
         elif key == Key.backspace:
             print("Turn Spot-cam Reset")
             reset()
@@ -176,9 +229,11 @@ def on_press(key):
             c = True
         elif key == Key.enter:
             if select_menu == " Image":
-                #global image_client
-                #image_response = image_client.get_image_from_sources(["left_fisheye_image"])[0]
+                global image_client
+                image_response = image_client.get_image_from_sources([camera])[0]
                 print("Capture Image with",camera)
+                image = Image.open(io.BytesIO(image_response.shot.image.data))
+                image.show()
             elif select_menu == " Ptz":
                 print("Capture Ptz")
     if r == False:
@@ -200,6 +255,7 @@ def on_release(key):
 
 def main():
     # Collect events until released
+    print("Ready")
     with keyboard.Listener(
                 on_press=on_press,
                 on_release=on_release) as listener:
